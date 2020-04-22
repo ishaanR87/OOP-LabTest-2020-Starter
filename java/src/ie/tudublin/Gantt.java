1df4be7e11b8d74e375d2da71584ec00826fc698
ishaan rawat <c18459302@mytudublin.ie>
2020-04-22 12:25:49 +0100

    Testing using map function in order to display task names on the left

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -37,6 +37,28 @@ public class Gantt extends PApplet
 			System.out.println(t);
 		}
 	}
+
+	public void displayTasks()
+	{
+		float border = width * 0.1f;
+		float left = width * 0.05f;
+
+		float w = width * 0.3f;
+		float h = height * 0.1f;
+
+		for(int i = 0; i < tasks.size(); i++)
+		{
+			Task t = tasks.get(i);
+
+			float y = map(i, 0, tasks.size(), border, height - border);
+			fill(0);
+			rect (left, y, w, h);
+			fill(220);
+			textAlign(LEFT, CENTER);
+			text(t.getTask(), left + 20, y + (h/3));
+		}
+	}
+
 	
 	public void mousePressed()
 	{
@@ -59,5 +81,6 @@ public class Gantt extends PApplet
 	public void draw()
 	{			
 		background(0);
+		displayTasks();
 	}
 }
